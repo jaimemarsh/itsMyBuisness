78ecd09d3eff9120359d3fcaa12540143b710a87
Jaime M <jaimemarsh19@gmail.com>
2023-04-19 13:22:47 -0400

    starting addEmloyees

---
+++ controllers/addEmployees.js
@@ -0,0 +1,22 @@
+const inquirer = require('inquirer');
+const connection = require('../config/db.js');
+
+const { getAllRoles, getAllManagers } = require('./getAll')
+
+const addEmployee = (askTask) => {
+
+    Promise.all([getAllRoles(), getAllManagers()])
+        .then(([allRoles, allManagers]))
+    inquirer.prompt([
+        {
+
+        }
+    ])
+
+
+
+
+
+
+
+}
