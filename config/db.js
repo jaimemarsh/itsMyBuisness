1a4e0b074837126f4d17f2c367e098c702b8ca58
Jaime M <jaimemarsh19@gmail.com>
2023-04-18 22:06:59 -0400

    Alan started helping, making progress

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+//connect to database
+const connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Emerald132!',
+        database: 'movies_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+);
+module.exports = connection;
---
+++ controllers/questions.js
@@ -0,0 +1,27 @@
+const inquirer = require('inquirer');
+
+const questions = [{
+    type: "list",
+    name: "task",
+    message: "What would you like to do?",
+    choices: [
+        new inquirer.Separator("===VIEW INFO ==="),
+        "View all employees",
+        'View employees by manager',
+        'View all roles',
+        'View all departments',
+        new inquirer.Separator('=== ADD NEW INFO ==='),
+        'Add employee',
+        'Add role',
+        'Add department',
+        new inquirer.Separator('=== UPDATE INFO ==='),
+        'Update employee',
+        new inquirer.Separator('=== DELETE INFO ==='),
+        'Delete employee',
+        'Delete role',
+        'Delete department',
+        new inquirer.Separator('=== EXIT ==='),
+        'Exit Application',
+    ]
+}]
+module.exports = questions;
