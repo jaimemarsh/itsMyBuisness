d77f07cf6e0945a3ccf842b0e1e33c960d534d12
Jaime M <jaimemarsh19@gmail.com>
2023-04-22 18:39:24 -0400

    prompt questions for employees

--- controllers/addEmployees.js
+++ controllers/addEmployees.js
@@ -9,7 +9,33 @@ const addEmployee = (askTask) => {
         .then(([allRoles, allManagers]))
     inquirer.prompt([
         {
-
+            type: 'input',
+            name: 'firstName',
+            message: "What is the employee's first name?",
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: "What is the employee's last name?",
+        },
+        {
+            type: 'list',
+            name: 'roleId',
+            message: "What is the employee's role?",
+            choices: allRoles
+        },
+        {
+            type: 'list',
+            name: 'managerOrNo',
+            message: "Does the employee have a manager?",
+            choices: ['yes', 'no'],
+        },
+        {
+            type: 'list',
+            name: 'managerId',
+            message: "Who is the employee's manager?",
+            choices: allManagers,
+            when: (answers) => answers.managerOrNo === 'yes',
         }
     ])
 
